8af2e36abfb98075f66dccdda38f92db46519006
Lucas <58853488+Walife14@users.noreply.github.com>
2021-12-11 02:07:36 +0000

    Added new notes functionality
    + can get item from clicking on it on the list displayed
    + can now show the selected note to the main note container

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Grabbing elements
 const collection = document.querySelector('.collection')
 const collectionItem = document.querySelectorAll('.collection-item')
+const noteContainer = document.querySelector('#note-container')
 
 console.log(localStorage)
 
@@ -13,8 +14,13 @@ const loopStorage = () => {
         // create the div with class collection-item
         let div = document.createElement("div")
         div.classList.add("collection-item")
-        div.innerHTML = localStorage.key(i)
 
+        // create span tag with class key-title
+        let span = document.createElement('span')
+        span.innerText = localStorage.key(i)
+
+        // add the span to the div
+        div.appendChild(span)
 
         // create i tag with the send icon and add to div
         let icon = document.createElement("i")
@@ -28,9 +34,31 @@ const loopStorage = () => {
         collection.appendChild(div)
 
         // add event listener to the item
-        div.addEventListener('click', () => console.log(div.innerText))
-        
+        div.addEventListener('click', () => changeDisplayedNote(span.innerText))
     }
+}
+
+const changeDisplayedNote = (key) => {
+    // removing the current note
+    noteContainer.innerHTML = ''
+
+    // grab the localStorage item selected
+    // the item we get in the function is already the title
+    const value = localStorage.getItem(key)
+    
+    // create the span for title and the new p tag for the body of the note
+    const span = document.createElement('span')
+    const p = document.createElement('p')
+
+    // add the title to the new span title and the class
+    span.innerHTML = key
+    span.classList.add('card-title')
+    // add the text to the p tag and also add the class to it
+    p.innerHTML = value
+    p.classList.add('card-text')
+    // append the title and body text to the noteContainer
+    noteContainer.appendChild(span)
+    noteContainer.appendChild(p)
 }
 
 // run functions
